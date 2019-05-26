56a550f7facb36d119831605a968de2d498a8617
sangchung11 <32933990+sangchung11@users.noreply.github.com>
2019-05-26 22:13:05 +0900

    Update schedule.h

--- schedule.h
+++ schedule.h
@@ -1,10 +1,22 @@
 #pragma once
 #include <stdio.h>
 
+#include "DrawSideMenu.h"
+#include "menu.h"
+#include "staff.h"
+#include "login.h"
+
+#define scheduleMenuNum 3
+#define InsertscheduleMenuNum 5
+#define DeletescheduleMenuNum 3
+
+void Print_scheduleinfo();
+
 void Schedule();
-int Check();
-void Print_Schedule_Menu();
+
+void Print_Insert_scheduleinfo();
 void Insert_Schedule();
+void Print_Delete_scheduleinfo();
 void Delete_Schedule();
 void Insert_outside();
 void Insert_day();
