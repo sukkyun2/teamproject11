f57826ded62e081150330ceb6a045a65984a816f
sangchung11 <32933990+sangchung11@users.noreply.github.com>
2019-05-26 22:12:42 +0900

    Update login.c

--- login.c
+++ login.c
@@ -1,118 +1,46 @@
 #include "login.h"
+#include "DrawLogin.h"
+
+typedef struct login {         // 로그인 구조체
+	char ID[10];
+	char PW[10];
+}login;
 
 void Login()
 {
-	login id = { "0000","0000" };		// 로그인 id 초기화
-	login *newid = (login *)malloc(sizeof(login));
-	Fullscreen();
+	login id = { "0000","0000" };      // 로그인 id 초기화
+	login* newid = (login*)malloc(sizeof(login));
+	setfFullscreen();
 	do {
 		system("cls");
-		Print_login();						// 로그인 화면 출력
+		Print_login();
 
-		screen(94, 25);
+		gotoxy(pos_start.X - 2, pos_start.Y - 2);
 		scanf("%s", newid->ID);
-		if (strcmp(newid->ID, "0000") == 0) {		// 회원 정보 수정
+		if (strcmp(newid->ID, "0000") == 0) {      // 회원 정보 수정
 			system("cls");
-			Print_ChangeID();					// 회원 정보 수정 화면 출력
-			screen(94, 24);
+			Print_ChangeID();               // 회원 정보 수정 화면 출력
+			gotoxy(pos_start.X - 2, pos_start.Y - 5);
 			printf("%s", id.ID);
-			screen(94, 25);
-			printf("%s", id.PW);				// 기존 id,pw 출력
-			screen(96, 27);
+			gotoxy(pos_start.X - 2, pos_start.Y - 3);
+			printf("%s", id.PW);            // 기존 id,pw 출력
+			gotoxy(pos_start.X - 2, pos_start.Y + 1);
 			scanf("%s", id.ID);
-			screen(96, 28);
+			gotoxy(pos_start.X - 2, pos_start.Y + 3);
 			scanf("%s", id.PW);
 		}
-		else {									// ID에 0000이 아니면
-			screen(94, 26);
+		else {                           // ID에 0000이 아니면
+			gotoxy(pos_start.X - 2, pos_start.Y);
 			scanf("%s", newid->PW);
-			if (strcmp(newid->ID, id.ID) != 0 || strcmp(newid->PW, id.PW) != 0) {	// id, pw가 일치하지않으면
+			if (strcmp(newid->ID, id.ID) != 0 || strcmp(newid->PW, id.PW) != 0) {   // id, pw가 일치하지않으면
 				system("cls");
-				screen(75, 23);
-				printf("ERR :: ID,PW를 잘못입력하셨습니다.");				// 오류메세지 출력
-				screen(75, 25);
+
+				gotoxy(pos_start.X - strlen("ERR :: ID,PW를 잘못입력하셨습니다.") / 2, pos_start.Y - 5);
+				printf("ERR :: ID,PW를 잘못입력하셨습니다.");            // 오류메세지 출력
+				gotoxy(pos_start.X - strlen("ERR :: ID,PW를 잘못입력하셨습니다.") / 2, pos_start.Y - 3);
 				system("PAUSE");
 			}
 		}
-	} while (strcmp(newid->ID, id.ID) != 0 || strcmp(newid->PW, id.PW) != 0);			// id,pw 일치할때까자지 반복
+	} while (strcmp(newid->ID, id.ID) != 0 || strcmp(newid->PW, id.PW) != 0);         // id,pw 일치할때까자지 반복
 
 }
-
-void screen(int x, int y) {
-	HANDLE screen = GetStdHandle(STD_OUTPUT_HANDLE);
-	COORD max_size = GetLargestConsoleWindowSize(screen);
-	COORD pos;
-	pos.X = x;
-	pos.Y = y;
-	SetConsoleCursorPosition(screen, pos);
-}
-
-void Fullscreen() {
-	SetConsoleDisplayMode(
-		GetStdHandle(STD_OUTPUT_HANDLE),
-		CONSOLE_FULLSCREEN_MODE,
-		0
-	);
-
-}
-
-void Print_login()
-{
-	system("cls");
-
-	screen(78, 20);
-	printf("   * 직원 일정 관리 프로그램 *");
-	screen(78, 21);
-	printf("---------------------------------");
-	screen(78, 22);
-	printf("|                               |");
-	screen(78, 23);
-	printf("|                               |");
-	screen(78, 24);
-	printf("|                               |");
-	screen(78, 25);
-	printf("|         ID :                  |");
-	screen(78, 26);
-	printf("|         PW :                  |");
-	screen(78, 27);
-	printf("|                               |");
-	screen(78, 28);
-	printf("|                               |");
-	screen(78, 29);
-	printf("|                               |");
-	screen(78, 30);
-	printf("---------------------------------");
-	screen(75, 32);
-	printf("* ID에 0000을 입력하시면 ID,PW를 수정할 수 있습니다.");
-
-}
-
-void Print_ChangeID()
-{
-	system("cls");
-
-	screen(78, 21);
-	printf("---------------------------------");
-	screen(78, 22);
-	printf("|                               |");
-	screen(78, 23);
-	printf("|                               |");
-	screen(78, 24);
-	printf("|         ID :                  |");
-	screen(78, 25);
-	printf("|         PW :                  |");
-	screen(78, 26);
-	printf("|                               |");
-	screen(78, 27);
-	printf("|        new ID :               |");
-	screen(78, 28);
-	printf("|        new PW :               |");
-	screen(78, 29);
-	printf("|                               |");
-	screen(78, 30);
-	printf("|                               |");
-	screen(78, 31);
-	printf("---------------------------------");
-
-}
-
