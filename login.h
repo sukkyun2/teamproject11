95bbe003d3588878d22708268d5f91be69b85142
sangchung11 <32933990+sangchung11@users.noreply.github.com>
2019-05-26 22:12:46 +0900

    Update login.h

--- login.h
+++ login.h
@@ -5,13 +5,8 @@
 #include <Windows.h>
 #include <string.h>
 
-typedef struct login {			// 로그인 구조체
-	char ID[10];
-	char PW[10];
-}login;
+#include"DrawCenterBox.h"
 
-void screen(int x, int y);
-void Fullscreen();
-void Print_ChangeID();
-void Print_login();
+#define _CRT_SECURE_NO_WARNINGS
+
 void Login();
